bdf95f8892fe6132409e626ff9419b1c2bc207f7
manuelmeyer <m.meyer@telaside.com>
2020-05-23 01:56:58 +0200

    more UT test

--- rti4t-xd-commons/src/test/java/com/dell/rti4t/xd/filter/TestLacCellReductionFilter.java
+++ rti4t-xd-commons/src/test/java/com/dell/rti4t/xd/filter/TestLacCellReductionFilter.java
@@ -35,6 +35,7 @@ public class TestLacCellReductionFilter {
 		DataTransporter dt;
 		dt = buildEvent(24610, 1339412, 1590067404);	
 		assertTrue(underTest.accept(dt));
+		assertDtEquals(dt, 24610, 1339412, 1590067404);
 		
 		dt = buildEvent(24610, 1339412, 1590069280);	
 		assertFalse(underTest.accept(dt));
@@ -45,13 +46,21 @@ public class TestLacCellReductionFilter {
 		
 		dt = buildEvent(9999, 9999, 1590069284);	
 		assertTrue(underTest.accept(dt));
-		LOG.info("Accepted dt {}", dt);
+		assertDtEquals(dt, 9999, 9999, 1590069284, 24610, 1339412, 1590069282);
 
 		dt = buildEvent(24610, 1339412, 1590069282);	
 		assertFalse(underTest.accept(dt));
 
 		dt = buildEvent(24611, 834324, 1590069299);
 		assertFalse(underTest.accept(dt));
+		
+		dt = buildEvent(24610, 1339412, 1591069299);
+		assertTrue(underTest.accept(dt));
+		assertDtEquals(dt, 24610, 1339412, 1591069299);
+
+		dt = buildEvent(9999, 0, 1591079299);
+		assertTrue(underTest.accept(dt));
+		assertDtEquals(dt, 9999, 0, 1591079299, 24610, 1339412, 1591069299);
 	}
 
 	@Test
@@ -73,24 +82,11 @@ public class TestLacCellReductionFilter {
 		
 		dt = buildEvent(10, 1, 60);	
 		assertTrue(underTest.accept(dt));
-		LOG.info("dt1 {}", dt);
-		assertEquals("10", dt.getFieldValue("lac"));
-		assertEquals("1", dt.getFieldValue("cellTower"));
-		assertEquals("60000", dt.getFieldValue("timeUTC"));
-		assertNull(dt.getFieldValue("previousLac"));
-		assertNull(dt.getFieldValue("previousCellTower"));
-		assertNull(dt.getFieldValue("previousTimeUTC"));
+		assertDtEquals(dt, 10, 1, 60);
 
 		dt = buildEvent(12, 5, 62);		
 		assertTrue(underTest.accept(dt));		
-		assertEquals("12", dt.getFieldValue("lac"));
-		assertEquals("5", dt.getFieldValue("cellTower"));
-		assertEquals("62000", dt.getFieldValue("timeUTC"));		
-		assertEquals("10", dt.getFieldValue("previousLac"));
-		assertEquals("1", dt.getFieldValue("previousCellTower"));
-		assertEquals("60000", dt.getFieldValue("previousTimeUTC"));
-
-		LOG.info("dt2 {}", dt);
+		assertDtEquals(dt, 12, 5, 62, 10, 1, 60);
 
 		dt = buildEvent(12, 5, 63);
 		assertFalse(underTest.accept(dt));
@@ -106,42 +102,59 @@ public class TestLacCellReductionFilter {
 		
 		dt = buildEvent(10, 1, 67);		
 		assertTrue(underTest.accept(dt));
-		LOG.info("dt-second {}", dt);
-		assertEquals("10", dt.getFieldValue("lac"));
-		assertEquals("1", dt.getFieldValue("cellTower"));
-		assertEquals("67000", dt.getFieldValue("timeUTC"));
-		assertEquals("12", dt.getFieldValue("previousLac"));
-		assertEquals("5", dt.getFieldValue("previousCellTower"));
-		assertEquals("66000", dt.getFieldValue("previousTimeUTC"));
+		assertDtEquals(dt, 10, 1, 67, 12, 5, 66);
 
 		dt = buildEvent(99, 99, 84);
 		assertTrue(underTest.accept(dt));
-		LOG.info("dt3 {}", dt);		
-		assertEquals("99", dt.getFieldValue("lac"));
-		assertEquals("99", dt.getFieldValue("cellTower"));
-		assertEquals("84000", dt.getFieldValue("timeUTC"));
-		assertEquals("10", dt.getFieldValue("previousLac"));
-		assertEquals("1", dt.getFieldValue("previousCellTower"));
-		assertEquals("67000", dt.getFieldValue("previousTimeUTC"));
+		assertDtEquals(dt, 99, 99, 84, 10, 1, 67);
 
 		dt = buildEvent(999, 999, 90);
 		assertFalse(underTest.accept(dt));
 		
 		dt = buildEvent(10, 1, 95);		
 		assertTrue(underTest.accept(dt));
-		LOG.info("dt4 {}", dt);
+		assertDtEquals(dt, 10, 1, 95);
 		
 		dt = buildEvent(999, 999, 100);
 		assertTrue(underTest.accept(dt));
-		LOG.info("dt5 {}", dt);
+		assertDtEquals(dt, 999, 999, 100, 10, 1, 95);
 		
 		dt = buildEvent(11, 11, 110);
 		assertTrue(underTest.accept(dt));
-		assertEquals("11", dt.getFieldValue("lac"));
-		assertEquals("11", dt.getFieldValue("cellTower"));
-		assertEquals("110000", dt.getFieldValue("timeUTC"));
-		assertNull(dt.getFieldValue("previousLac"));
-		assertNull(dt.getFieldValue("previousCellTower"));
-		assertNull(dt.getFieldValue("previousTimeUTC"));
+		assertDtEquals(dt, 11, 11, 110);
+		
+		dt = buildEvent(100, 0, 120);
+		assertTrue(underTest.accept(dt));
+		assertDtEquals(dt, 100, 0, 120, 11, 11, 110);
+
+		dt = buildEvent(999, 999, 130);
+		assertTrue(underTest.accept(dt));
+		assertDtEquals(dt, 999, 999, 130, 100, 0, 120);
 	}
+	
+	private void assertDtEquals(DataTransporter dt, Integer lac, Integer cell, Integer timeUTC) {
+		assertDtEquals(dt, lac, cell, timeUTC, null, null, null);
+	}
+	
+	private void assertDtEquals(DataTransporter dt, Integer lac, Integer cell, Integer timeUTC, Integer formerLac, Integer formerCell, Integer formerTimeUTC) {
+		assertEquals(String.valueOf(lac), dt.getFieldValue("lac"));
+		assertEquals(String.valueOf(cell), dt.getFieldValue("cellTower"));
+		assertEquals(String.valueOf(timeUTC) + "000", dt.getFieldValue("timeUTC"));
+		asssertNullOrEquals(formerLac, dt.getFieldValue("previousLac"));
+		asssertNullOrEquals(formerCell, dt.getFieldValue("previousCellTower"));
+		asssertNullOrEquals(nullOrAdd(formerTimeUTC, "000"), dt.getFieldValue("previousTimeUTC"));
+	}
+
+	private String nullOrAdd(Integer value, String suffix) {
+		return value == null ? null : String.valueOf(value) + suffix;
+	}
+
+	private void asssertNullOrEquals(Object value, String fieldValue) {
+		if(value != null) {
+			assertEquals(String.valueOf(value), fieldValue);
+		} else {
+			assertNull(fieldValue);
+		}
+	}
+
 }
